c404785e35668052b093dcbf49a41b596958de96
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-11-30 21:10:21 +0900

    ✨ Feature: ListItem 단일 아이템 컴포넌트 분리 및 ListItemList 구조 준비

--- src/components/list-item/ListItem.tsx
+++ src/components/list-item/ListItem.tsx
@@ -8,20 +8,18 @@ export type ListItemProps = {
 
 export default function ListItem({ item, onDeleteWord }: ListItemProps) {
   return (
-    <>
-      <li
-        className="px-3 py-3 text-sm border border-dashed rounded-lg border-main"
-        key={item.id}
-      >
-        <p>단어: {item.word}</p>
-        <p>뜻: {item.meaning}</p>
-        <p>작문: {item.sentence}</p>
-        <p>사용처: {item.usage}</p>
+    <li
+      className="px-3 py-3 text-sm border border-dashed rounded-lg border-main"
+      key={item.id}
+    >
+      <p>단어: {item.word}</p>
+      <p>뜻: {item.meaning}</p>
+      <p>작문: {item.sentence}</p>
+      <p>사용처: {item.usage}</p>
 
-        <Button onClick={() => onDeleteWord(item.id)} variant="small">
-          삭제 꾹 🐾
-        </Button>
-      </li>
-    </>
+      <Button onClick={() => onDeleteWord(item.id)} variant="small">
+        삭제 꾹 🐾
+      </Button>
+    </li>
   );
 }
