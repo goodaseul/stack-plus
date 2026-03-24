2a98343656121725973dd38c9b65a30b1f0011ae
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2026-03-24 20:49:10 +0900

    style: 글꼴 반응형에 맞게 수정

--- src/app/(public)/landing-page/_components/SectionWrapper.tsx
+++ src/app/(public)/landing-page/_components/SectionWrapper.tsx
@@ -13,10 +13,10 @@ export default function SectionWrapper({
       className={`mx-auto w-full text-center bg-background text-foreground py-30`}
     >
       <div className="m-auto max-w-6xl flex flex-col justify-center px-4">
-        <h2 className="break-keep text-3xl font-bold tracking-tight leading-normal md:text-4xl mb-5">
+        <h2 className="break-keep text-2xl font-bold tracking-tight leading-normal md:text-4xl mb-2 md:mb-5">
           {title}
         </h2>
-        <p className="break-keep text-base leading-relaxed text-muted sm:text-lg">
+        <p className="break-keep text-md leading-relaxed text-muted sm:text-lg">
           {desc}
         </p>
         {children}
--- src/app/(public)/landing-page/_components/banner/Banner.tsx
+++ src/app/(public)/landing-page/_components/banner/Banner.tsx
@@ -2,7 +2,7 @@ export default function BannerPage() {
   return (
     <section className="text-background bg-point py-20">
       <div className="mx-auto max-w-3xl px-6 text-center">
-        <h2 className="break-keep text-2xl font-bold tracking-tight">
+        <h2 className="break-keep text-xl md:text-2xl font-bold tracking-tight">
           오늘 배운 표현부터
           <br />
           <span className="font-sekuya">StackPlus</span> 에 차곡차곡 쌓아보세요.
